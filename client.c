9bbed90332c3fd88d8f6e50f58184456a039ce5b
schultz15u <schultz.claire@free.fr>
2017-04-18 09:48:53 +0200

    Ajout du client

--- client.c
+++ client.c
@@ -1,1 +1,34 @@
 #include "client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+
+int main(int argc, char **argv) {
+
+    int clientSocket;
+    struct sockaddr_in serv_addr;
+
+    if ((clientSocket = socket(PF_INET, SOCK_STREAM, 0)) <0 ) {
+        perror ("erreur socket");
+        exit(1);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons((ushort)atoi(argv[2]));
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    if (connect(clientSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) <0) {
+        perror("cliecho : erreur connect");
+        exit(1);
+    }
+
+    return(0);
+
+}
