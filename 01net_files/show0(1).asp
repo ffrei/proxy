bf2af87379b9900b4c7cc77d8749ca882b7e1b59
schultz15u <schultz.claire@free.fr>
2017-04-19 23:48:33 +0200

    Ajout de adBloc.c -> suppression des fenetre avec comme nom de classe onf-ad. ATTENTION lire ALire.txt impérativement!!!

---
+++ 01net_files/show0(1).asp
@@ -0,0 +1,12 @@
+ 
+
+var innerCode='';
+
+innerCode = innerCode + '<a target="_blank"  href="http://securite.01net.com/diff/13/4585226/go0.asp?4585226%3B45414%3B6234263989375149083%3B9047043991%3BS%3Bsystemtarget=%24qc%3D1307971523%3B%24ql%3Dmedium%3B%24qpc%3D75001%3B%24qpp%3D0%3B%24qt%3D184_1903_42652t%3B%24dma%3D0%3B%24b%3D16570%3B%24o%3D99999%3Btarget%3D%24iab%3D19t%3B%24dt%3D1t%3Bamznslots%3B%24hc%3B13814479%3Bclickvars="><img id="img13814479" src="http://ced.sascdn.com/diff/13/4585226/mea-app-01net.jpg" alt="" width=304 height=102 border="0"></a>';
+
+
+
+innerCode = innerCode + '\r\n';
+
+
+if(typeof(sas_ajax)!='undefined'&&sas_ajax){sas_appendToContainer(6458,innerCode);}else{document.write(innerCode);}
---
+++ adBloc.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "myhtml-4.0.0/source/myhtml/api.h"
+
+#include "myhtml-4.0.0/examples/myhtml/example.h"
+
+#define DIE(msg, ...) do { fprintf(stderr, msg, ##__VA_ARGS__); exit(EXIT_FAILURE); } while(0)
+
+char* classKey = "class";
+char* addValue = "onf-ad";
+
+static bool filter_node(myhtml_tree_node_t* node)
+{
+    assert(node);
+    myhtml_tag_id_t tag = myhtml_node_tag_id(node);
+    return (tag != MyHTML_TAG__TEXT) && (tag != MyHTML_TAG__END_OF_FILE) && (tag != MyHTML_TAG__COMMENT) && (tag != MyHTML_TAG__UNDEF);
+}
+
+/* depth-first lefthand tree walk */
+static void walk_subtree(myhtml_tree_t* tree, myhtml_tree_node_t* root, int level)
+{
+    if (!root) {
+        printf("pas de root");
+        return;
+    }
+
+    /* Check if we handle this node type */
+    if (!filter_node(root)) {
+        //printf("On ne gère pas les noeuds : %s \n", myhtml_tag_name_by_id(tree, myhtml_node_tag_id(root), NULL));
+        return;
+    }
+
+    /* start sexpr */
+    //putchar('(');
+
+    /* print this node */
+    //printf("%s ", myhtml_tag_name_by_id(tree, myhtml_node_tag_id(root), NULL));
+    myhtml_tree_attr_t* attr = myhtml_node_attribute_first(root);
+    while (attr != NULL) {
+        /* attribute sexpr (name value)*/
+        const char *key = myhtml_attribute_key(attr, NULL);
+        const char *value = myhtml_attribute_value(attr, NULL);
+
+        if ((key!=NULL) && (value!=NULL)) {
+          if((strcmp(key, classKey)==0) && (strcmp(value, addValue)==0)) {
+            myhtml_node_delete_recursive(root);
+            root = NULL;
+          }
+        }
+        /*if (key==NULL)
+            printf("(KEY IS NULL)");
+        else if (value)
+            printf("(%s \'%s\')", key, value);
+        else
+            printf("(%s)", key);*/
+
+        attr = myhtml_attribute_next(attr);
+    }
+
+    /* left hand depth-first recoursion */
+    if (root != NULL){
+      myhtml_tree_node_t* child = myhtml_node_child(root);
+      while (child != NULL) {
+          myhtml_tree_node_t* nextChild = myhtml_node_next(child);
+          walk_subtree(tree, child, level + 1);
+          child = nextChild;
+      }
+    }
+
+
+    /* close sexpr */
+    //printf("\n");
+    //putchar(')');
+}
+
+struct res_html {
+    char  *html;
+    size_t size;
+};
+
+struct res_html load_html_file(const char* filename)
+{
+    FILE *fh = fopen(filename, "rb");
+    if(fh == NULL) {
+        DIE("Can't open html file: %s\n", filename);
+    }
+
+    if(fseek(fh, 0L, SEEK_END) != 0) {
+        DIE("Can't set position (fseek) in file: %s\n", filename);
+    }
+
+    long size = ftell(fh);
+
+    if(fseek(fh, 0L, SEEK_SET) != 0) {
+        DIE("Can't set position (fseek) in file: %s\n", filename);
+    }
+
+    if(size <= 0) {
+        fclose(fh);
+
+        struct res_html res = {NULL, 0};
+        return res;
+    }
+
+    char *html = (char*)malloc(size + 1);
+    if(html == NULL) {
+        DIE("Can't allocate mem for html file: %s\n", filename);
+    }
+
+    size_t nread = fread(html, 1, size, fh);
+    if (nread != size) {
+        DIE("could not read %ld bytes (" MyCORE_FMT_Z " bytes done)\n", size, nread);
+    }
+
+    fclose(fh);
+
+    struct res_html res = {html, (size_t)size};
+    return res;
+}
+
+static void usage(void)
+{
+    fprintf(stderr, "html2sexpr <file>\n");
+}
+
+mystatus_t write_output(const char* data, size_t len, void* ctx)
+{
+    printf("%.*s", (int)len, data);
+    return MyCORE_STATUS_OK;
+}
+
+int main(int argc,char *argv[]) {
+  if (argc != 2) {
+    usage();
+    DIE("Invalid number of arguments\n");
+  }
+
+  struct res_html data = load_html_file(argv[1]);
+  mystatus_t res;
+
+	// basic init
+  myhtml_t* myhtml = myhtml_create();
+  if (!myhtml) {
+    DIE("myhtml_create failed\n");
+  }
+
+  res = myhtml_init(myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+  if (MYHTML_FAILED(res)) {
+  	DIE("myhtml_init failed with %d\n", res);
+  }
+
+  // init tree
+  myhtml_tree_t* tree = myhtml_tree_create();
+  if (!tree) {
+    DIE("myhtml_tree_create failed\n");
+  }
+
+  res = myhtml_tree_init(tree, myhtml);
+  if (MYHTML_FAILED(res)) {
+  	DIE("myhtml_tree_init failed with %d\n", res);
+  }
+
+  // parse html
+  myhtml_parse(tree, MyENCODING_UTF_8, data.html, data.size);
+  printf("fin parse\n");
+  walk_subtree(tree, myhtml_tree_get_node_html(tree), 0);
+  printf("fin subtree\n");
+  printf("\n");
+  myhtml_serialization_tree_callback(myhtml_tree_get_document(tree), write_output, NULL);
+  // release resources
+  myhtml_tree_destroy(tree);
+  myhtml_destroy(myhtml);
+  free(data.html);
+
+  return EXIT_SUCCESS;
+}
