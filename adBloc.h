5681eeb19e6c2b1938ef17f7f58db05c7812c0b7
schultz15u <schultz.claire@free.fr>
2017-04-20 13:27:38 +0200

    Modification adBloc pour prendre char* en parametres et rendre char*. Ajout .h

---
+++ adBloc.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "myhtml-4.0.0/source/myhtml/api.h"
+
+#include "myhtml-4.0.0/examples/myhtml/example.h"
+
+#ifndef ADBLOC_H_INCLUDED
+#define ADBLOC_H_INCLUDED
+
+char* cleanAd(struct res_html data);
+
+#endif
